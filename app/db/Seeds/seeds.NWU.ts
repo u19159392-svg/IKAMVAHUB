099ed7aba04dd83a76f4df030c4f5abcdaefc1f4
RevelationM <kealebogarevelation@gmail.com>
2026-08-10 18:49:39 +0200

    Added all seed files for all universities and Colleges (Team Member 6)

---
+++ app/db/Seeds/seeds.NWU.ts
@@ -0,0 +1,580 @@
+export const northWestUniversityQualifications = [
+  {
+    //==== Faculty of Economic and Management Science ====//
+  faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Accounting Sciences",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: 4,
+  mathematics: 6,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Vanderbijlpark Campus"
+}, 
+
+{
+  faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Chartered Accountancy",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 32,
+  english: 4,
+  mathematics: 5,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng, Potchefstroom and Vanderbijlpark Campuses"
+},
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Chartered Accountancy Extended Programmer",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 28,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+}, 
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Financial Accountancy",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 28,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng, Potchefstroom and Vanderbijlpark Campuses"
+}, 
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Financial Accountancy Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 24,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: 7,
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+},
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Forensic Accountancy",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 36,
+  english: 5,
+  mathematics: "Level 5(60-69%) if student took Grade 12 Accounting or level 6(70-79%) if student did not take Grade 12 accounting",
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Potchefstroom Campus"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Accountancy",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 30,
+  english: 4,
+  mathematics: 5,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Potchefstroom  and Vanderbijlpark Campuses"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Operations Research",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in statistics",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng and Potchefstroom Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in statistics Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 20,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Business Operations with Transport Economics",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Business Operations with Logistics Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Business Operations with Logistics Management Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 20,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: 7,
+  additionalRequirements: "Offered at Mahikeng  and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Business Operations with Transport Economics",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng  and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Business Operations with Transport Economics Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 20,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Agriclutural Economics and Risk Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 26,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Potchefstroom Campus"
+},
+
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Econometrics",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 26,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Econometrics Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 20,
+  english: 4,
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with International Trade ",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 26,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Potchefstroom and Vanderbijlpark Campus"
+},
+{
+faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with International Trade Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 24,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Information Systems",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 26,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Information Systems Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 20,
+  english: 4,
+  mathematics: 4,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+}, 
+
+{
+   faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Risk Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 26,
+  english: "Not Stipulated",
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Potchefstroom and Vanderbijlpark Campus"
+},
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Economic Science with Risk Management Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Administration(BAdmin) in Human Resource Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 23,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Administration(BAdmin) in Human Resource Management Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 21,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Administration(BAdmin) in Industrial and Organisational Psychology",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 23,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Administration(BAdmin) in Industrial and Organisational Psychology Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 21,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Arts (BA) with Industrial and Organisational Psychology and Labour Relations Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 26,
+  english: "Not Stipulated",
+  mathematics: 4,
+  mathematicalLiteracy: 5,
+  additionalRequirements: "Offered at Potchefstroom and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Human Resource Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 30,
+  english: "Not Stipulated",
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng, Potchefstroom and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Industrial and Organisational Psychology",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 30,
+  english: "Not Stipulated",
+  mathematics: 4,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng, Potchefstroom and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Human Resource Development(BHRD) in Human Resource Development",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 20/22,
+  english: "Not Stipulated",
+  mathematics: "Not Stipulated",
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "A minimum of three years of working experience as a trainer in the Training and Development environment OR attained a minimum APS of 22, as well as a minimum one year of working experience as a trainer in the Training and Development. Offered at Potchefstroom, Limited contact-study school"
+},
+
+{
+   faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Communication Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Mahikeng, Potchefstroom and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Marketing Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng, Potchefstroom and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Marketing Management Extended Programme",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 20,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Sport Business Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Safety Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Marketing and Tourism Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Mahikeng and Vanderbijlpark Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Arts with Tourism Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 22,
+  english: "Not Stipulated",
+  mathematics: "Not Stipulated",
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Mahikeng Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Tourism Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Offered at Potchefstroom Campus"
+},
+
+{
+    faculty: "Economic and Management Science",
+  qualification: "Bachelor of Commerce(BCom) in Management Sciences with Tourism and Recreation Skills",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Not Stipulated",
+  mathematics: 3,
+  mathematicalLiteracy: 6,
+  additionalRequirements: "Offered at Potchefstroom Campus"
+},
+
+
+// ===== Faculty of Health Sciences ===== //
+
+{
+    faculty: "Health ",
+  qualification: "Diploma in Coaching Sciences Sport Coaching",
+  type: "Diploma",
+  duration: "2 years",
+  aps: 18,
+  english: "Level 4 for language of tuition, academic paper selection, best average mark",
+  mathematics: "Not Stipulated",
+  mathematicalLiteracy: "Not Stipulated",
+  additionalRequirements: "Offered at Mahikeng and Potchefstroom Campus: Full-time Contact"
+},
+
+{
+    faculty: "Health ",
+  qualification: "Bachelor of Health Sciences with Sport Coaching and Human Movement Scinece",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Level 4 for language of tuition, academic paper selection, best average mark",
+  mathematics: "Not Stipulated",
+  mathematicalLiteracy: "Not Stipulated",
+  additionalRequirements: "Offered at Mahikeng and Potchefstroom Campus: Full-time Contact"
+},
+
+{
+    faculty: "Health ",
+  qualification: "Bachelor of Health Sciences with Recreation Science and Tourism Management",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Level 4 for language of tuition, academic paper selection, best average mark",
+  mathematics: "Not Stipulated",
+  mathematicalLiteracy: "Not Stipulated",
+  additionalRequirements: "Offered at Potchefstroom Campus: Full-time Contact"
+},
+
+{
+    faculty: "Health ",
+  qualification: "Bachelor of Health Sciences with Sport and Recreation Administration",
+  type: "Bachelor's Degree",
+  duration: "3 years",
+  aps: 24,
+  english: "Level 4 for language of tuition, academic paper selection, best average mark",
+  mathematics: "Not Stipulated",
+  mathematicalLiteracy: "Not Stipulated",
+  additionalRequirements: "Offered at Potchefstroom Campus: Full-time Contact"
+},
+
+{
+    faculty: "Health ",
+  qualification: "Bachelor of Health Sciences in Biokinetics ",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 32,
+  english: "Level 4 for language of tuition, academic paper selection, best average mark",
+  mathematics: 3,
+  mathematicalLiteracy: 4,
+  additionalRequirements: "Physical Science OR Life Sciences at level 4. Offered at Potchefstroom Campus: Full-time Contact"
+},
+
+{
+    faculty: "Health ",
+  qualification: "Bachelor of Pharmacy",
+  type: "Bachelor's Degree",
+  duration: "4 years",
+  aps: 32,
+  english: "Level 4 for language of tuition,academic paper selection and best average mark",
+  mathematics: 5,
+  mathematicalLiteracy: "Not Accepted",
+  additionalRequirements: "Physical Science at level 5. Late applications will be considered on merit, depending on capacity.Offered at Potchefstroom Campus: Full-time Contact",
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
